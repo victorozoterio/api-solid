9c65dbaa508f3cafd3f29e230fbc25cb8143d9d5
Victor Ozoterio <victorozoterio@gmail.com>
2025-06-09 09:53:03 -0300

    feat: configura o prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
-import fastify from 'fastify'
+import fastify from "fastify";
+import { PrismaClient } from "generated/prisma";
 
-export const app = fastify()
+export const app = fastify();
+
+const prisma = new PrismaClient();
+
+prisma.user.create({
+  data: {
+    name: "Victor Ozoterio",
+    email: "victorozoterio@gmail.com",
+  },
+});
