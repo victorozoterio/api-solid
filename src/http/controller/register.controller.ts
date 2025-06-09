756e986680314495cb436bb85b70e2d026560722
Victor Ozoterio <victorozoterio@gmail.com>
2025-06-09 18:08:37 -0300

    feat: hasheia a senha do usuário antes de salvar no banco de dados

--- src/http/controller/register.controller.ts
+++ src/http/controller/register.controller.ts
@@ -1,4 +1,5 @@
 import { FastifyRequest, FastifyReply } from "fastify";
+import { hash } from "bcryptjs";
 import { prisma } from "@/lib/prisma";
 import { z } from "zod";
 
@@ -10,13 +11,13 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   });
 
   const { name, email, password } = registerBodySchema.parse(request.body);
+  const passwordHash = await hash(password, 6);
+
+  const emailAlreadyExists = await prisma.user.findUnique({ where: { email } });
+  if (emailAlreadyExists) return reply.status(409).send();
 
   await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash: password,
-    },
+    data: { name, email, password_hash: passwordHash },
   });
 
   return reply.status(201).send();
