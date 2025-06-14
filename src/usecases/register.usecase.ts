8b94df14075d7f78a5c31d56ac233dbe36e6b747
Victor Ozoterio <victorozoterio@gmail.com>
2025-06-13 22:21:20 -0300

    test: adiciona teste unitário para hash da senha no caso de uso de registros

--- src/usecases/register.usecase.ts
+++ src/usecases/register.usecase.ts
@@ -1,6 +1,7 @@
 import { UsersRepository } from "@/repositories/users.repository";
 import { hash } from "bcryptjs";
 import { UserAlreadyExistsError } from "./errors/user-already-exists.error";
+import { User } from "generated/prisma";
 
 interface RegisterUseCaseRequest {
   name: string;
@@ -8,19 +9,29 @@ interface RegisterUseCaseRequest {
   password: string;
 }
 
+interface RegisterUseCaseResponse {
+  user: User;
+}
+
 export class RegisterUseCase {
   constructor(private usersRepository: UsersRepository) {}
 
-  async execute({ name, email, password }: RegisterUseCaseRequest) {
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
     const passwordHash = await hash(password, 6);
 
     const emailAlreadyExists = await this.usersRepository.findByEmail(email);
     if (emailAlreadyExists) throw new UserAlreadyExistsError();
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash: passwordHash,
     });
+
+    return { user };
   }
 }
