7d1407ac0cc911aa54615a2bccd87a5640152fe7
Victor Ozoterio <victorozoterio@gmail.com>
2025-06-19 10:56:48 -0300

    feat: cria controller de autenticação

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,9 @@
 import { FastifyInstance } from "fastify";
 import { register } from "./controller/register.controller";
+import { authenticate } from "./controller/authenticate.controller";
 
 export async function appRoutes(app: FastifyInstance) {
   app.post("/users", register);
+
+  app.post("/sessions", authenticate);
 }
