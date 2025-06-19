187fe5c61827acff365db9ff2fa4f8c9e497c2b3
Victor Ozoterio <victorozoterio@gmail.com>
2025-06-19 15:47:00 -0300

    feat: valida data do check-in impossibilitando mais de um check-in no mesmo dia

--- src/repositories/in-memory/in-memory-check-ins.repository.ts
+++ src/repositories/in-memory/in-memory-check-ins.repository.ts
@@ -1,6 +1,7 @@
 import { Prisma, CheckIn } from "generated/prisma";
 import { CheckInsRepository } from "@/repositories/check-ins.repository";
 import { randomUUID } from "node:crypto";
+import dayjs from "dayjs";
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = [];
@@ -16,5 +17,22 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
 
     this.items.push(checkIn);
     return checkIn;
+  }
+
+  async findByUserIdOnDate(userId: string, date: Date) {
+    const startOfTheDay = dayjs(date).startOf("date");
+    const endOfTheDay = dayjs(date).endOf("date");
+
+    const checkInOnSameDate = this.items.find((checkIn) => {
+      const checkInDate = dayjs(checkIn.created_at);
+      const inOnSameDate =
+        checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay);
+
+      return checkIn.user_id === userId && inOnSameDate;
+    });
+
+    if (!checkInOnSameDate) return null;
+
+    return checkInOnSameDate;
   }
 }
